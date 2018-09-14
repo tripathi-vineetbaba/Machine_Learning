d8ebec34ebd9b9540eb6716f38742c4af7130f4a
tripathi-vineetbaba <tripathi.vineetbaba@gmail.com>
2018-09-15 01:43:48 +0530

    Updating sklearn algo to accept data from a file.

--- Machine_Learning/code/iris_LinearRegression_sklearn/iris_model_train_and_predict.py
+++ Machine_Learning/code/iris_LinearRegression_sklearn/iris_model_train_and_predict.py
@@ -13,22 +13,29 @@ df=df.values
 X=df[:,:-1]
 Y=df[:,-1]
 print('*****************************************************************************************************')
-
+#b'../../../../New_folder/testfile.csv'
 #all_result=algos.get_accurqacy_all_algorithm(X,Y)
 #print("\n\tAccuracy of algorithm on given IRIS data is as below :\n{0}".format(all_result))
 #fit_algorithm=algos.get_fit_algorithm(X,Y)
 #print("\n\tAlgorithm that suites most with this type of data :\n{0}".format(fit_algorithm))
+
+testfile=input("\n\tEnter file name with file location : ")
+read_file=pd.read_csv(testfile,sep=',',names=column)
+read_file=read_file.values
+testY=read_file[:,-1]
+testX=read_file[:,:-1]
+#test-file is at ../../../../New_folder/testfile.csv
 algorithm,accuracy=algos.apply_fit_algorithm(X,Y)
-print("\n\tAlgorithm that is applied : \n{0}".format(algorithm))
-trainX,testX,trainY,testY=model_selection.train_test_split(X,Y,test_size=0.2,random_state=np.random.randint(len(X)))
+print("\n\tAlgorithm that is applied : \n\t\t{0}".format(algorithm))
+trainX,test_X,trainY,test_Y=model_selection.train_test_split(X,Y,test_size=0.2,random_state=np.random.randint(len(X)))
 algorithm.fit(trainX,trainY)
 prediction=algorithm.predict(testX)
-print('*****************************************************************************************************')
+print('\n***********************************************PREDICTION AND ACTUAL VALUE************************************************\n')
 for x in range(len(prediction)):
     if testY[x]=='Iris-setosa':
         print("Predicted Class : {0}\t\t\t {2} : {1}".format(prediction[x],testY[x],'Actual Class'))
     else:
         print("Predicted Class : {0}\t\t {2} : {1}".format(prediction[x],testY[x],'Actual Class'))
-print("*****************************************************************************************************\nOverall Accuracy : {0}".format(accuracy))
+print("\n*************************************************ACCURACY****************************************\nOverall Accuracy : {0}".format(accuracy))
 
 time.sleep(30)
